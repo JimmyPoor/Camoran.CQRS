7694dca884c93a0fc95ce443768eda5e10d3ac34
huachaoZheng <kissnana5@live.cn>
2017-07-11 00:01:26 +0800

    Design command and handler

--- Core/ICommandBus.cs
+++ Core/ICommandBus.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Camoran.CQRS.Core
 {
     public interface ICommandBus
     {
-    }
+       void  SendCommand<TCommand>(TCommand command) where TCommand:ICommand;
+       Task  SendCommandAsync<TCommand>(TCommand command);
+
+       ConcurrentDictionary<ICommand,ICommandHandler> Commands { get;}
+    }   
 }
---
+++ Core/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Camoran.CQRS.Core
+{
+    public interface ICommandHandler
+    {
+        void SendCommand<ICommand>(ICommand message);
+    }
+}
